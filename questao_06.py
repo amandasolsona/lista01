da949a521d3ca4285acec09acbb4d96d7ee914ea
Amanda Solsona <amandasolsona0@gmail.com>
2023-05-09 16:44:50 -0300

    Questão 06

---
+++ questao_06.py
@@ -0,0 +1,8 @@
+#Exercício do Cálculo de Área: Escreva um programa em Python que solicite ao usuário a largura e a altura
+#de um retângulo. Calcule a área desse retângulo e exiba o resultado.
+
+largura = float(input('Qual é sua largura ?'))
+altura = float(input('Qual é a sua altura ?'))
+area = largura*altura
+
+print(f'A área desse retângulo é {area}')
