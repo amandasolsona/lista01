9c638f13fe52467d00399358d413d1061e2353de
Amanda Solsona <amandasolsona0@gmail.com>
2023-05-09 14:38:12 -0300

    Questão 01 - 01 Lista de Lógica Programação

---
+++ questao_01.py
@@ -0,0 +1,6 @@
+#Crie um programa com uma mensagem pessoal que você ama programar em Python.
+# Imprima essa mensagem em letras maiúsculas e depois em minúsculas.
+
+frase = "Eu amo programar em Python !"
+print(frase.upper())
+print(frase.lower())
