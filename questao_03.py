3da89fc5f68e21480e6bd1c9b71049b96d024206
Amanda Solsona <amandasolsona0@gmail.com>
2023-05-09 15:43:49 -0300

    Alteração na questão 03

--- questao_03.py
+++ questao_03.py
@@ -5,4 +5,4 @@ nota_1 = float(input('Qual foi sua primeira nota ?'))
 nota_2 = float(input('Qual foi sua segunda nota ?'))
 media = (nota_1 + nota_2 ) / 2
 
-print(media)
+print(f'Sua média é {media}')
