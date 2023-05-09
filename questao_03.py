cd51af034228a9a0e5193e5c68140d8b56087daa
Amanda Solsona <amandasolsona0@gmail.com>
2023-05-09 15:35:27 -0300

    Questão 03

---
+++ questao_03.py
@@ -0,0 +1,8 @@
+# Exercício da Média: Escreva um programa em Python que solicite ao usuário duas notas e calcule a média entre elas.
+# Em seguida, exiba o resultado na tela.
+
+nota_1 = float(input('Qual foi sua primeira nota ?'))
+nota_2 = float(input('Qual foi sua segunda nota ?'))
+media = (nota_1 + nota_2 ) / 2
+
+print(media)
